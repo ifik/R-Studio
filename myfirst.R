95cb465d50fae9621f349474aa227d031637b694
Ifik Arifin <ifikarifin@gmail.com>
2020-06-13 11:39:53 +0700

    Ini selagi saya belajar membaca file text, csv dan lainnya

--- myfirst.R
+++ myfirst.R
@@ -1,2 +1,17 @@
 print ("My R-Studio")
 
+lulus <- c("Lulus", "Tidak Lulus","Lulus","Lulus","Tidak Lulus")
+
+df <- data.frame (
+  nama = c("Ahmad", "Berti", "Cesar","Doei"),
+  gender= c("L","P","L","L"),
+  nilai = c(78,85,65,77),
+  lulus = c(T,T,F,T)  
+)
+df <- data.frame(
+  x = 1:3,
+  y = c("a", "b", "c"),
+  stringsAsFactors = T)
+str(df)
+
+  
