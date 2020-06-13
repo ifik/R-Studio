54690e85c4c6340f0f6dc1de7f53ad5e4620fc52
Ifik Arifin <ifikarifin@gmail.com>
2020-06-13 19:05:20 +0700

    Memasukkan subsetting dari data frame, vektor, list dan matrix

--- myfirst.R
+++ myfirst.R
@@ -14,4 +14,16 @@ df <- data.frame(
   stringsAsFactors = T)
 str(df)
 
-  
+daftar<- read.table("makanan.txt",header=T)
+daftar
+typeof(daftar)
+str(daftar)
+class(daftar)
+daftar$gaplek
+
+r <- read.table("http://www.bio.ic.ac.uk/research/mjcraw/therbook/data/cancer.txt",header=T)
+head(r)
+kota<-c("Jakrta","Badung","Surabaya","Batam","Makassar","Jogja")
+
+
+
