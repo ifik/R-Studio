706336b0eb67d0c09af5020963ad897261777ccd
Ifik Arifin <ifikarifin@gmail.com>
2020-06-14 10:21:45 +0700

    memasukan unsur subsetting

--- myfirst.R
+++ myfirst.R
@@ -23,7 +23,29 @@ daftar$gaplek
 
 r <- read.table("http://www.bio.ic.ac.uk/research/mjcraw/therbook/data/cancer.txt",header=T)
 head(r)
-kota<-c("Jakrta","Badung","Surabaya","Batam","Makassar","Jogja")
+kota<-c("Jakarta","Badung","Surabaya","Batam","Makassar","Jogja")
+kota[c(3,1)]
+kota[c(3,2)]
+kota[c(5,4,3,2)]
+kota[order(kota)]
+kota[ -c(3,1)]
+kota[c(T,T,F,F,T,T)]
+kota[c(T,F)]
 
+myfunc <- function (a,b) {
+  # multiplikasi dan penambahan
+  a * b + 2
+}
+myfunc(20,30)
+formals(myfunc)
+body(myfunc)
+environment(myfunc)
+attr(myfunc,"srcref")
+df<- read.csv("hw1_data.csv")
+summary(filter(oz, ! is.na(Ozone)))
 
+ds<- filter(df, Ozone>31 & Temp>90)
+summary(ds$Solar.R)
 
+ds2 <- filter(df, Month==6)
+summary(ds2)
